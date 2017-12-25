aa2b619e00dc9a5195134f29776f8f1701dee9d5
Ron <ronmada@s.braude.ac.il>
2017-12-25 20:25:53 +0200

    commited

--- Prototype/src/catalog/ProductFormController.java
+++ Prototype/src/catalog/ProductFormController.java
@@ -48,8 +48,8 @@ public class ProductFormController implements Initializable
 	public void loadProduct(Product p1){
 		this.p=p1;
 		this.txtProductID.setText(p.get_product_ID());
-		this.txtProductName.setText(p.get_product_Name());//dsfsdsd
-		this.txtProductType.setText(p.get_product_Type());///dfdfsdfsdffgfgsdfsdf
+		this.txtProductName.setText(p.get_product_Name());
+		this.txtProductType.setText(p.get_product_Type());
 	}
 	
 	public void closeProductInfo(ActionEvent event) throws Exception {
